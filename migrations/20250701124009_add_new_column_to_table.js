5313b5e25c458228f6cb99967d19b14f8dd09452
Jen <jennifer.cc.su@gmail.com>
2025-07-22 16:07:13 +0800

    fix: update user migration and seed data to include user_name and user_email fields

--- migrations/20250701124009_add_new_column_to_table.js
+++ migrations/20250701124009_add_new_column_to_table.js
@@ -2,25 +2,34 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = async function up(knex) {
+export async function up(knex) {
   await knex.schema.table('users', (table) => {
-    table.string('user_name', 128).notNullable().defaultTo('').unique();
-    table.string('user_email', 128).notNullable().defaultTo('').unique();
-    table.string('user_password', 128).notNullable().defaultTo('').unique();
+    table.string('user_name', 128);
+    table.string('user_email', 128);
+    table.string('user_password', 128);
   });
-  return knex('users').insert([
-    { user_name: 'jen', user_email: 'jen@hotmail.com', user_password: '12' },
-  ]);
-};
-
+  const usersToUpdate = await knex('users').select('id').whereNull('user_name');
+  await Promise.all(
+    usersToUpdate.map((user) => knex('users')
+      .where('id', user.id)
+      .update({
+        user_name: `user_${user.name}`,
+        user_email: `user_${user.name}@hotmail.com`,
+      })),
+  );
+  await knex.schema.alterTable('users', (table) => {
+    table.string('user_name', 128).notNullable().unique().alter();
+    table.string('user_email', 128).notNullable().unique().alter();
+  });
+}
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = async function down(knex) {
+export async function down(knex) {
   return knex.schema.table('users', (table) => {
     table.dropColumn('user_name');
     table.dropColumn('user_email');
     table.dropColumn('user_password');
   });
-};
+}
--- seeds/create-test-users.js
+++ seeds/create-test-users.js
@@ -6,7 +6,17 @@ exports.seed = async function seed(knex) {
   // Deletes ALL existing entries
   await knex('users').del();
   await knex('users').insert([
-    { id: 1, name: 'jen' },
-    { id: 2, name: 'bill' },
+    {
+      id: 1,
+      name: 'jen',
+      email: 'jen@hotmail.com',
+      password: '123321',
+    },
+    {
+      id: 2,
+      name: 'bill',
+      email: 'bill@hotmail.com',
+      password: '234432',
+    },
   ]);
 };
