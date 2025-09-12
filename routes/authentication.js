d0ebe7437fcf39d9563645b5c49aa0a8e2a1302b
Jen <jennifer.cc.su@gmail.com>
2025-09-12 14:02:04 +0800

    refactor: remove unused passport import from authentication routes

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const passport = require('passport');
 const authenticationController = require('../controllers/authentication');
 
 const router = express.Router();
