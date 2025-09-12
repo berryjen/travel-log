7b5880145c97d8496679011898d8b6454758877a
Jen <jennifer.cc.su@gmail.com>
2025-09-12 14:19:56 +0800

    refactor: clean up comments and remove unnecessary whitespace in tokens controller

--- controllers/tokens.js
+++ controllers/tokens.js
@@ -7,11 +7,10 @@ exports.list = async (req, res) => {
 };
 
 exports.get = async (req, res) => {
-  
   const user = await usersModel.get_by_name(req.params.user_name);
   // this comes through as a request via user name, which is in request param
   const token = await tokensModel.get_token_by_user_id(user.id);
-  // using the name the user provides, it goes into db to find the matching id, which is then used to fetch the token via the id from the tokens table
-  
+  // using the name the user provides, it goes into db to find matching id
+  //  which is then used to fetch the token via the id from the tokens table
   return res.json(token);
 };
