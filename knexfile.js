988017fb60ffabf1b90d81f5f2908d6d929c080d
Jen <jennifer.cc.su@gmail.com>
2026-05-15 12:33:57 +0800

    chore: configure knexfile for SSH deployment for better CD/CI

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3',
+      filename: process.env.SQLITE_FILENAME || './dev.sqlite3',
     },
     useNullAsDefault: true,
   },
