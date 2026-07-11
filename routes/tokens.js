48b9c5b1fd146471129fb700e4ebbe68017ed082
Jen <jennifer.cc.su@gmail.com>
2026-07-11 16:54:00 +0800

    refactor: remove unused token routes and seed file

--- routes/tokens.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const tokensController = require('../controllers/tokens');
-
-const router = express.Router();
-
-router.get('/', tokensController.list);
-router.get('/:user_name', tokensController.get);
-module.exports = router;
