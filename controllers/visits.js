0c8494a0e6900186e8b9823aa9e619f2999e6076
Jen <jennifer.cc.su@gmail.com>
2026-08-02 14:07:13 +0800

    fix: normalize visit data in controller, add error handling, and clean up logs

--- controllers/visits.js
+++ controllers/visits.js
@@ -1,65 +1,80 @@
+/* eslint-disable consistent-return */
 const db = require('../db/db');
 const visitsModel = require('../models/visits');
 
-exports.list = async (req, res) => {
-  const visits = await visitsModel.get_all(req.user.id);
-  return res.json(visits);
+const normalizeVisitBody = (body) => ({
+  country_id: body.country_id ?? body.countryId,
+  arrival_time: body.arrival_time ?? body.arrivalTime,
+  departure_time: body.departure_time ?? body.departureTime,
+});
+
+exports.list = async (req, res, next) => {
+  try {
+    const visits = await visitsModel.get_all(req.user.id);
+    const normalizedVisits = visits.map(normalizeVisitBody);
+    return res.json(normalizedVisits);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.get = async (req, res) => {
-  const parsedId = parseInt(req.params.id, 10);
-  const visit = await visitsModel.get_by_id(parsedId, req.user.id);
-  console.log('visit from get req controller', visit);
-  return res.json(visit);
+exports.get = async (req, res, next) => {
+  try {
+    const parsedId = parseInt(req.params.id, 10);
+    const visit = await visitsModel.get_by_id(parsedId, req.user.id);
+    console.log('visit from get req controller', visit);
+    const normalizedVisit = normalizeVisitBody(visit);
+    return res.json(normalizedVisit);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.create = async (req, res) => {
-  console.log('visits controller create timestamp', new Date().toISOString());
-  console.log('visits.create req', req.body);
-  console.log('visits controller req', req.user, req.session.user);
-  console.log('visits.create res', res.body);
+exports.create = async (req, res, next) => {
   if (!req.user) {
     console.log('req user', req.user);
     return res.status(401).json({ message: 'Unauthorized' });
   }
   try {
-    console.log(
-      'visits controller create req info:',
-      req.body.name,
-      req.query.userEmail,
-      typeof req.body.userId,
-      req.body,
-    );
+    const body = normalizeVisitBody(req.body);
     // TODO: figure out a way to check that id isn't contained in body at all
     if (req.body.id !== undefined) {
       console.log('req.body.id from create visit controller', req.body.id);
-      return res.status(400).send('Bad Reqest, should not include id');
+      return res.status(400).json({ message: 'Bad Reqest, should not include id' });
+    }
+    if (body.country_id === undefined || body.country_id === null) {
+      return res.status(400).json({ message: 'countryId is required' });
     }
 
-    const country = await db('countries').where({ id: req.body.country_id });
+    const country = await db('countries').where({ id: body.country_id }).first();
+    if (!country) {
+      return res.status(400).json({ message: 'Invalid country' });
+    }
     const visit = await visitsModel.create(
       req.user.id,
-      country[0].id,
+      country.id,
+      // country[0].id,
       req.body.arrival_time,
       req.body.departure_time,
     );
     console.log('visits Model create', visit);
-    return res.status(201).json({ message: 'visit created successfully' });
+    return res.status(201).json(visit);
   } catch (err) {
     if (err instanceof visitsModel.ConstraintIdNullError) {
-      err.status = 400;
-    } else {
-      console.log('unable to create visit:', err);
-      err.message = 'unable to create visit';
+      return res.status(400).json({ message: 'Bad Request' });
     }
-    throw err;
+    next(err);
   }
 };
 
-exports.delete = async (req, res) => {
-  const deletedVisit = await visitsModel.delete_by_id(req.params.id);
-  if (deletedVisit === 0) {
-    return res.sendStatus(404);
+exports.delete = async (req, res, next) => {
+  try {
+    const deletedVisit = await visitsModel.delete_by_id(req.params.id);
+    if (deletedVisit === 0) {
+      return res.sendStatus(404);
+    }
+    return res.sendStatus(204);
+  } catch (err) {
+    next(err);
   }
-  return res.sendStatus(204);
 };
